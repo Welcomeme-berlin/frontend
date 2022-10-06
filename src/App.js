bc1d9df60df22ee0a3bee729c95c563b69b5eb8c
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2022-10-06 13:54:49 +0100

    configured tailwindcss and font, added assets folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Welcomme.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline text-center m-8">Welcome Me Frontend.</h1>
     </div>
   );
 }
