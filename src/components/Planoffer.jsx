1c8b3d6a5226c1ed4fef03773bf9dd82caec26e8
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2022-11-25 10:29:51 +0100

    update ui

---
+++ src/components/Planoffer.jsx
@@ -0,0 +1,23 @@
+const Planoffer = ({ offers }) => {
+  return (
+    <div>
+      <div className="w-full max-w-[578px] h-[80px] bg-slate-100 text-center font-bold pt-6">
+        <p>{offers.Type}</p>
+      </div>
+      {offers.parts.map((part) => {
+        return (
+          <div className="flex text-xl w-[500px]">
+            <div>
+              <part.IconTitle />
+            </div>
+            <div className="w-100%">
+              <h3>{part.title}</h3>
+              <p>{part.details[0]}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default Planoffer;
